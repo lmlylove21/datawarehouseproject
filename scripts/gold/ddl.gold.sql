b6ed601f13c61f8cf7008402b1b0d29644da1d1d
lmlylove21 <somyapal06@gmail.com>
2026-06-03 16:14:40 +0530

    Update ddl.gold.sql

--- scripts/gold/ddl.gold.sql
+++ scripts/gold/ddl.gold.sql
@@ -23,18 +23,18 @@ SET QUOTED_IDENTIFIER ON
 GO
 create view [gold].[dim_customers] as 
 select
-Row_number() over (order by c1.cust_id) as customer_key,
-c1.cust_id as customer_id,
-c1.cst_key as customer_number,
-c1.cst_firstname as first_name,
-c1.cst_lastname as last_name,
-c1.cst_marital_status as marital_status,
-case when  c1.cst_gnder!='n/a' then c1.cst_gnder
-else coalesce(cu.gen,'n/a')
-end as gender,
-c1.cst_create_date as create_date,
-l1.cntry as country,
-try_cast(cu.BDATE as date) as birth_date
+    Row_number() over (order by c1.cust_id) as customer_key,
+    c1.cust_id as customer_id,
+    c1.cst_key as customer_number,
+    c1.cst_firstname as first_name,
+    c1.cst_lastname as last_name,
+    c1.cst_marital_status as marital_status,
+    case when  c1.cst_gnder!='n/a' then c1.cst_gnder
+    else coalesce(cu.gen,'n/a')
+    end as gender,
+    c1.cst_create_date as create_date,
+    l1.cntry as country,
+    try_cast(cu.BDATE as date) as birth_date
 from silver.crm_cust_info as c1
 left join silver.erp_LOC_A101 as l1
 on C1.cst_key=l1.CID
@@ -73,15 +73,15 @@ SET QUOTED_IDENTIFIER ON
 GO
 create view [gold].[fact_sales] as
 select 
-sd.sls_ord_num as order_number,
-pr.product_key ,--dimesion key
-cu.customer_key,--dimension key
-sd.sls_order_dt as order_date,
-sd.sls_ship_dt as ship_date,
-sd.sls_due_dt as due_date,
-sd.sls_sales as sales,
-sd.sls_quantity as quantity,
-sd.sls_price as price
+    sd.sls_ord_num as order_number,
+    pr.product_key ,--dimesion key
+    cu.customer_key,--dimension key
+    sd.sls_order_dt as order_date,
+    sd.sls_ship_dt as ship_date,
+    sd.sls_due_dt as due_date,
+    sd.sls_sales as sales,
+    sd.sls_quantity as quantity,
+    sd.sls_price as price
 from silver.crm_sales_details sd
 left join gold.dim_products pr
 on pr.product_number =sd.sls_prd_key
